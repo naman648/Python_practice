d6d1dad3cc55f785d0f2c8b2cd1448e0ddb8fae5
Naman <naman648@example.com>
2026-03-19 23:44:57 +0530

    Boolean Condition

---
+++ day-01/q04.py
@@ -0,0 +1,31 @@
+'''5. Print the output of following statements 
+If 
+x = 2 
+y = 5 
+z = 0 
+then find values of the following expressions: 
+a. x == 2 
+b. x != 5 
+c. x != 5 && y >= 5 
+d. z != 0 || x == 2 
+e. !(y < 10) '''
+
+print("---BOOLEAN CONDITIONS CHECK THROUGH STATEMENTS---")
+
+x=2
+y=5
+z=0
+print("x==2:")
+print(x==2)
+
+print("x!=5: ")
+print(x!=5)
+
+print("x != 5 and y >= 5 : ")
+print(x != 5 and y >= 5 )
+
+print("z != 0 or x == 2 : ")
+print(z != 0 or x == 2 )
+
+print("not(y < 10): ")
+print(not(y < 10))
